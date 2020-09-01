9696c7b8f4b1eca758413326de3a51d83d736a43
NiekSulter <nieksulter99@gmail.com>
2020-09-01 20:56:50 +0200

    Initial commit

---
+++ src/textField.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class textField{
+    public static void main(String[] args) {
+        String inputString;
+
+        inputString = JOptionPane.showInputDialog("Vul je text in:");
+        JOptionPane.showMessageDialog(null,inputString);
+    }
+}
